67c251bc75921682b4ed07274f476cae521532b6
Swendude <swenmulderij@gmail.com>
2021-12-23 12:16:21 +0100

    Added navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import DiscoverPage from "./Pages/DiscoverPage";
 import AboutPage from "./Pages/AboutPage";
 import HomePage from "./Pages/HomePage";
+import NavBar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <h1>Hello React Router</h1>
       <Routes>
         <Route path="/discover" element={<DiscoverPage />} />
